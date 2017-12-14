665f91721262f7295e9c830405f0d1de88e8b12e
bcameron1489 <bcameron1489@gmail.com>
2017-12-14 11:02:21 -0500

    Add Resource Orders
    Add orders resource to app/models (schema)
    Add routes for orders
    Add controller orders.js
    
    brian cameron
    Contributions: shadow Peter, Derek

---
+++ app/models/order.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  products: [{
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    }
+  }],
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+module.exports = Order
